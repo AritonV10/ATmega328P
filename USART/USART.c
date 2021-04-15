f824e38aa074cdc4de1f27ca9a4b25aa55a249c4
AritonV10 <ariton.vio@gmail.com>
2021-04-15 23:34:22 +0300

    Update USART.c

--- USART/USART.c
+++ USART/USART.c
@@ -26,6 +26,12 @@
 /* 16MHz */
 #define USART__FCLK 16e6
 
+
+/* 32 bytes */
+#define USART__nRxBufferSize 0x20u
+#define USART__nTxBufferSize 0x20u
+
+
 #define USART__nMinBits 0x05u
 #define USART__nMaxBits 0x09u
 
@@ -37,19 +43,67 @@
 
 #define USART__u32AsyncNormalBaudRate(Baud) (((uint32_t)((USART__FCLK/USART__nAsyncNormalSampling) * 1/Baud)) - 1u)
 
+
+
+
 /*****************************************************************************************/
 /**************************** Private Function Declarations ******************************/
+
+
+/*****************************************************************************************/
+/**************************** Private Declarations ***************************************/
+
+
+
+#if defined USART_nDataBits_Cfg <= USART_nDataBits8
+  static volatile uint8_t USART__au8RxBuffer[USART__nRxBufferSize];
+  static volatile uint8_t USART__au8TxBuffer[USART__nTxBufferSize];
+#else
+  static volatile uint16_t USART__au16RxBuffer[USART__nRxBufferSize];
+  static volatile uint16_t USART__au16TxBuffer[USART__nTxBufferSize];
+#endif
+
+static volatile uint8_t USART__u8RxIdx;
+static volatile uint8_t USART__u8RxIdxISR;
+
+static volatile uint8_t USART__u8TxIdx;
+static volatile uint8_t USART__u8TxIdxISR;
+
 
 /*****************************************************************************************/
 /**************************** Public Function Definitions ********************************/
 
+
+
+#if defined USART_AsyncISR_Cfg == 1
+  
+  ISR(USART0_RX_vect) {
+    
+    uint8_t u8Status;
+    uint8_t u8Byte;
+    
+    u8Status = USART__UCSR0A;
+    u8Byte   = USART__UDR0;
+        
+     /* Check for errors */
+     if(u8Status & USART__nErrorMask)
+        return;
+     
+     USART__au8RxBuffer[USART__u8RxIdxISR];
+     USART__u8RxIdxISR = ((USART__u8RxIdxISR + 1) & (USART__nRxBufferSize - 1));
+     
+  }
+#endif
 #if defined USART_Async_Cfg == 1
+
   void
   USART_vInit(void) {
-  
+
+    USART__vDisableISR();
+    
     USART__UCSR0C = 0;
     USART__UCSR0B = 0;
-    
+
     /* Configure the baud rate */
     USART__UBRR0L = (uint8_t)(USART__u32AsyncNormalBaudRate(USART_nBaudRate_Cfg) & 0x000000FFu);
     USART__UBRR0H = (uint8_t)(USART__u32AsyncNormalBaudRate(USART_nBaudRate_Cfg) >> 8);
@@ -66,7 +120,12 @@
   
     /* Configure stop bits (3) */
     USART__UCSR0C |= (USART_nStopBits_Cfg << USART__nUSBS0);
-  
+
+    #if defined USART_AsyncISR_Cfg == 1
+      USART__UCSR0B = (1u << USART__nRXCIE0);
+    #endif
+
+    USART__vEnableISR();
   
   }
   
@@ -74,23 +133,39 @@
   
     uint8_t
     USART_u8Read(void) {
-      
-      uint8_t u8Status;
       uint8_t u8Byte;
-          
       
-      /* Busy wait until data is received */
-      while (!(USART__UCSR0A & (1u << USART__nRXC0)))
-        ;
+      #if defined USART_AsyncISR_Cfg == 1
+         
+         /* Waiting for data */
+         while( USART__u8RxIdx == USART__u8RxIdxISR )
+          ;
+          /* Get the byte from the Rx buffer */
+         u8Byte = USART__au8RxBuffer[USART__u8RxIdx];
+
+         /* Wraparound*/
+         USART__u8RxIdx = ((USART__u8RxIdx + 1) & (USART__nRxBufferSize - 1));
+
+         return(u8Byte);
+         
+      #else
       
-      u8Status = USART__UCSR0A;
-      u8Byte   = USART__UDR0;
+        uint8_t u8Status;
+        
+        /* Busy wait until data is received */
+        while (!(USART__UCSR0A & (1u << USART__nRXC0)))
+          ;
+        
+        u8Status = USART__UCSR0A;
+        u8Byte   = USART__UDR0;
+        
+        /* Check for errors */
+        if(u8Status & USART__nErrorMask)
+          return(-1);
+        
+        return (u8Byte);
+      #endif
       
-      /* Check for errors */
-      if(u8Status & USART__nErrorMask)
-        return(-1);
-      
-      return (u8Byte);
     }
     
     void
@@ -147,4 +222,17 @@
     }
   #endif
 
+  void
+  USART_vSendString(uint8_t *puStr) {
+    
+    for(; *puStr != 0; ++puStr)
+      USART_vSend(*puStr);
+    
+  }
+
+  void
+  USART_vSendU32(uint32_t u32Data) {
+    
+  }
+  
 #endif
