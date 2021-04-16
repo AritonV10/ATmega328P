d0fcc74c3894794943f361ac3415f4171c5c5a25
AritonV10 <ariton.vio@gmail.com>
2021-04-16 20:23:32 +0300

    Update USART_Cfg.h

--- USART/USART_Cfg.h
+++ USART/USART_Cfg.h
@@ -1,34 +1,50 @@
 #ifndef USART_CFG__H__GUARD
 #define USART_CFG__H__GUARD
 
+  #include "USART_Def.h"  
+
+
+  /*****************************************************************************************/
+  /********************************* USART Configuration ***********************************/
+ 
   #define USART_nBaudRate_Cfg 9600
   #define USART_nDataBits_Cfg USART_nDataBits8
-
   /*
    * 1: Tx
    * 2: Rx
    * 3: RxTx
    */
   #define USART_nTranmission_Cfg 3
+  
   /* 
    * 0: Disabled
    * 2: Even
    * 3: Odd
    */
   #define USART_nParity_Cfg 0
+  
   /*
    * 0: 1 SB
    * 1: 2 SB
    */
   #define USART_nStopBits_Cfg 1
+  
+  
+  #define USART_Async_Cfg                1
+  #define USART_AsyncNormalMode_Cfg      1
+  #define USART_AsyncDoubleMode_Cfg      0
 
+  /*
+   * ISR Enabler
+   */
+  #define USART_AsyncISR_Cfg             1
 
-  #define USART_Async_Cfg           1
-  #define USART_AsyncNormalMode_Cfg 1
-  #define USART_AsyncDoubleMode_Cfg 0
-
-  #define USART_ISR_AsyncNormalMode_Cfg 1;
-
+  /*
+   * 0: Data overwrite
+   * 1: No data overwrite
+   */
+  #define USART_AsyncISR_BufferError_Cfg 1
+  
   /* Data bits configurations */
   #define USART_nDataBits5 0x00u
   #define USART_nDataBits6 0x01u
